ca8d27d2cff78cb5e2ced48757e0eee888eeca51
xiaoxi <1223696182@qq.com>
2019-08-16 19:46:59 +0800

    初始化项目第一次

---
+++ XiaoXiSwift/XiaoXiSwift/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  XiaoXiSwift
+//
+//  Created by XiaoXi on 2019/8/16.
+//  Copyright © 2019 XiaoXi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
